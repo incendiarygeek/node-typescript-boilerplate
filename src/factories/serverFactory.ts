f1dfaae0ba42e880b27bd3ae6ddd1d7e7e9b656c
Huzaifa Hassan <huzaifa.hassan2612@gmail.com>
2022-10-04 20:48:44 +0500

    refactored code

--- src/factories/serverFactory.ts
+++ src/factories/serverFactory.ts
@@ -1,6 +1,5 @@
 /* library imports */
 import express, { Express } from 'express';
-// import httpErrors from 'http-errors';
 import path from 'path';
 import cookieparser from 'cookie-parser';
 import http from 'http';
@@ -13,19 +12,12 @@ export interface envInterface {
   build: 'dev' | 'release' | ''
 
 }
-export default class serverFactory
+export default class serverFactory extends http.Server
 {
-  on ( arg0: string, arg1: () => void )
-  {
-    throw new Error( 'Method not implemented.' );
-  }
-  listen ( arg0: number, arg1: string )
-  {
-    throw new Error( 'Method not implemented.' );
-  }
+
   constructor ( {app}: {app:Express} )
   {
-    // console.log( `port: ${port}; build: ${build}` );
+    super()
     app.use(express.json());
     app.use(cookieparser());
     app.use(express.urlencoded({ extended: false }));
@@ -35,7 +27,7 @@ export default class serverFactory
     /*  insert routes  */
     app.get( '/', ( req, res ) =>
     {
-      console.log( req )
+      // console.log( req )
       res.send('root directory')
     } )
     app.use(function (
@@ -44,7 +36,7 @@ export default class serverFactory
       res:
       {
         locals: { message: string; error: { message?: string; status?: string } };
-        status: (arg0: any) => void;
+        status: (arg0: unknown) => void;
         render: (arg0: string) => void;
       },
     ) {
@@ -56,23 +48,8 @@ export default class serverFactory
       res.status(err.status || 500);
       res.render('error');
     } );
-    // this.app.use( function ( req: Request, res: Response, next )
-    // {
-    //   res.locals.message = 'Request:' + req.body + 'Response: error 404';
-    //   next(httpErrors(404));
-    // });
     const server = http.createServer(app);
-    // server.listen(3000, '127.0.0.1');
-    // server.on('listening', () => {
-    //   console.log('listening on port 3000');
-    // } );
     return server as http.Server
   }
 
 }
-// const app = new serverFactory( { port: 3000, build: 'dev' } );
-// const server = http.createServer(app as unknown as Express);
-// server.listen(3000, '127.0.0.1');
-// server.on('listening', () => {
-//   console.log('listening on port 3000');
-// });
